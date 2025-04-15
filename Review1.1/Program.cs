c062aa830c03baa89002c810b8bccbf30196d407
denzelsilvestre <denzelsilvestre@DENZELSILVE132D>
2025-04-14 19:01:19 -0500

    Add project files.

---
+++ Review1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Information Input");
+
+Console.WriteLine("what is your first name?");
+string firstname = Console.ReadLine();//first name
+
+Console.WriteLine("what is your last name?");
+string lastname = Console.ReadLine();//last name
+
+
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());// age
+
+
+Console.WriteLine("What is your favorate color?");
+string color = Console.ReadLine();// color
+
+
+
+
+Console.WriteLine("Hello," + firstname + lastname + "! You are" + age + "year old and your favorite color is " + color);
