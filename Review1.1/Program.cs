b4e91ea164c659fff87d4201cd01831d6b1944a1
dsilvestreperez <dsilvestreperez@amarillocollege.com>
2025-04-20 19:34:21 -0500

    Update Program.cs

--- Review1.1/Program.cs
+++ Review1.1/Program.cs
@@ -1,21 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Information Input");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine("what is your first name?");
-string firstname = Console.ReadLine();//first name
-
-Console.WriteLine("what is your last name?");
-string lastname = Console.ReadLine();//last name
-
-
-Console.WriteLine("Enter your age:");
-int age = Convert.ToInt32(Console.ReadLine());// age
-
-
-Console.WriteLine("What is your favorate color?");
-string color = Console.ReadLine();// color
+namespace Consoledocumation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
 
 
+            Console.WriteLine("Information Input");
+
+            Console.WriteLine("what is your first name?");// input firstname
+            string firstname = Console.ReadLine();//first name
+
+            Console.WriteLine("what is your last name?");//input lastname
+            string lastname = Console.ReadLine();//last name
 
 
-Console.WriteLine("Hello," + firstname + lastname + "! You are" + age + "year old and your favorite color is " + color);
+            Console.WriteLine("Enter your age:");//input age
+            int age = Convert.ToInt32(Console.ReadLine());// age
+
+
+            Console.WriteLine("What is your favorate color?");//input color 
+            string color = Console.ReadLine();// color
+
+
+            Console.WriteLine("Hello," + firstname + lastname + "! You are" + age + " year old and your favorite color is " + color);// output
+
+
+        }
+
+    }
+
+}
